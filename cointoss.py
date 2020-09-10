be9690cfefa74ba6dae51a10f2cf4a3576023a25
zebbaukhagen <69488628+zebbaukhagen@users.noreply.github.com>
2020-09-10 18:54:16 -0400

    Add cointoss via upload

---
+++ cointoss.py
@@ -0,0 +1,20 @@
+import random
+
+def cointosspc():
+    number = input("How many times to flip a coin?: ")
+    heads = 0
+    tails = 0
+    for amount in range(int(number)):
+        flip = random.randint(0, 1)
+        if flip == 0:
+            print("Heads")
+            heads += 1
+        else:
+            print("Tails")
+            tails += 1
+    percentage = heads / int(number)
+    print("{} heads and {} tails".format(heads, tails))
+    print("Percentage of heads is {:.0%}".format(percentage))
+
+
+cointosspc()
